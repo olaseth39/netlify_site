ee0d7c75eedc8e582afa53ee06eda86142310423
Abiola Daropale <olaseth39@gmail.com>
2025-08-20 13:57:13 +0100

    updated file

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
 
-  backend "remote" {            # tells Terraform where to store its state file remotely
-    organization = "dev_seth"   # HCP Terraform organization name
-    workspaces {
+  cloud {                         # tells Terraform where to store its state file remotely
+      organization = "dev_seth"   # HCP Terraform organization name
+      workspaces {
       name = "netlify-site"
     }
   }
@@ -19,14 +19,14 @@ provider "netlify" {
   token = var.netlify_token  # Netlify API token
 }
 
-resource "netlify_site" "example" {
-  name   = var.my_terraform_site    # name of the Netlify site
-  custom_domain = "" # leave empty for now, will set custom domain later
+data "netlify_site" "example" {
+  name = var.site_name
 }
+
 
 # deployment
 resource "netlify_deploy" "example" {
-  site_id    = netlify_site.example.id
+  site_id    = data.netlify_site.example.id
   file       = "${path.module}/site.zip"    # upload the zip folder
   draft      = false
   functions  = null
