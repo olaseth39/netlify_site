f8710ed07dd405c0ad62e102fb58a3f431b92501
Abiola Daropale <olaseth39@gmail.com>
2025-08-20 15:02:10 +0100

    updated file

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "site_url" {
   description = "The Netlify live site URL"
-  value       = data.netlify_site.example.ssl_url
+  value = "https://${data.netlify_site.example.custom_domain != "" ? data.netlify_site.example.custom_domain : "${data.netlify_site.example.name}.netlify.app"}"
 }
 
 
